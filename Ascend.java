d8dc7ceeacd2e85c4a555d93dd9ffcd1efc49236
Or Pravda <orpravda@Ors-MacBook-Pro.local>
2024-11-09 22:35:57 +0200

    done

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,20 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) ((Math.random() * lim));
+		int b = (int) ((Math.random() * lim));
+		int c = (int) ((Math.random() * lim));
+		System.out.println(a + " " + b + " " + c);
+
+		int min = Math.min(a, b);
+		min = Math.min(min, c) ; // finding the min number
+		int max = Math.max(a, b);
+		max = Math.max(max, c); // finding the max number
+		int mid = (a + b + c) - (max + min); // calculating the middle number
+		System.out.println(min + " " + mid + " " + max);
+
+
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		int hundreds = number/100;
+		int tens = (number/10) - (hundreds * 10);
+		int ones = number - ((tens*10) + (hundreds * 100)) ;
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
